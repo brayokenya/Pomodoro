05f77d53ab28c4cc842b9874986b2c03d0109b27
brayokenya <kiirubrian21@gmail.com>
2020-06-10 14:58:12 +0300

    added routes

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+bootstrap = Bootstrap()
+def create_app(config_name):
+    # initialize app
+    app = Flask(__name__)
+    #create app configurations
+    app.config.from_object(config_options[config_name])
+    bootstrap.init_app(app)
+    #registering blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    return app
